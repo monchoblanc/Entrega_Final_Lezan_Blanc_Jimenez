0b00d7c7e13ef7bc9ef060c57e54b0c7a9636ad1
monchoblanc <monchoblanc@gmail.com>
2022-08-08 21:57:02 -0300

    commit inicial

--- accounts/models.py
+++ accounts/models.py
@@ -1,20 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import User #hace falta o viene con la linea de arriba?
+from django.contrib.auth.models import User 
 #para poder linkear User con Perfil:
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# Create your models here.
+# LOS MODELOS:
 
-#mi RegisterForm me crea usuarios de User. aca intento extender al la info del Profile. 
+#mi RegisterForm me crea usuarios de User. aca extiendo, para poder tener la info del Profile. 
 class Perfil(models.Model):
-    user= models.OneToOneField(User, on_delete=models.CASCADE, null=True) #q tenga los fields de User, mas los q agrego abajo. null=True hace falta?
-    imagen= models.ImageField(upload_to='avatar', blank=True, null=True) # pide instalar Pillow. BLANK=TRUE, para q el ModelForm me tome required=False en el ModelForm!
-    descripcion= models.TextField(blank=True, null=True) #default es Textarea...
+    user= models.OneToOneField(User, on_delete=models.CASCADE, null=True) #q tenga los fields de User, mas los q agrego abajo.
+    imagen= models.ImageField(upload_to='avatar', blank=True, null=True) #BLANK=TRUE, para q el ModelForm me tome required=False en el ModelForm!
+    descripcion= models.TextField(blank=True, null=True) 
     link= models.URLField(max_length=200, blank=True, null=True) 
 
     def __str__(self):
-        return str(self.user) #+'  '+self.contrasena+'  '+str(self.email)+'  '+str(self.link) #ROMPE.....asiq lo dejo asi.
+        return str(self.user) #con este me alcanza.
     
 
 
@@ -24,11 +24,12 @@ def create_user_perfil(sender, instance, created, **kwargs):
     if created:
         Perfil.objects.create(user=instance)
 
+#este signal me chillaba y no lo estoy necesitando. 
 '''@receiver(post_save, sender=User) #si guardo un User me guarda el Perfil
 def save_user_perfil(sender, instance, **kwargs): # uso save_user_profile. 
     instance.Perfil.save()   '''
 
-#hace falta o no xq ya tengo el campo ImageField en Perfil!
+#hace falta o no?ya esta todo funcionando, asiq lo dejo sin.
 '''class Avatar(models.Model):
     user=models.ForeignKey(User, on_delete=models.Cascade)
     imagen'''
--- accounts/urls.py
+++ accounts/urls.py
@@ -7,9 +7,8 @@ urlpatterns = [
     path('login/', login_request, name='login'),
     path('signup/', register, name='signup'), #ojo notar la vista es register, no signup. 
     path('logout/', LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
-    path('profile/update_perfil', update_perfil, name='update_perfil'), #estoy cambiando p agregar la nueva vista ver_perfil.
+    path('profile/update_perfil', update_perfil, name='update_perfil'), 
     path('deleteUser/<usuario>', deleteUser, name='deleteUser'),
-    path('profile/', ver_perfil, name='ver_perfil'),
-    path('perfil/', info_perfil, name='info_perfil'),
+    path('profile/', info_perfil, name='info_perfil'),
     
 ]
